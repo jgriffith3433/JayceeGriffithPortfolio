9ba0f1bf6ccc19176b531a65ec955dde0214b94c
jgriffith3433 <44073795+jgriffith3433@users.noreply.github.com>
2019-01-28 16:21:49 -0800

    initial commit, working publish profile, took out login from boiler plate template

---
+++ JayceeGriffithPortfolio/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace JayceeGriffithPortfolio
+{
+    public static class Constants
+    {
+        public static string AuthorizationCookieKey => "Auth";
+        public static string HttpContextServiceUserItemKey => "ServiceUser";
+    }
+}
---
+++ JayceeGriffithPortfolio/Models/ServiceUser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JayceeGriffithPortfolio.Models
+{
+    public class ServiceUser
+    {
+        public string Login { get; set; }
+    }
+}
